e8ecb60b82f65ac5123c17e69b2e9cb0402d18f8
libarb <pontiuspilate36@gmail.com>
2023-12-06 06:47:08 +0300

    class

---
+++ surpo/surpo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace surpo
+{
+    class Cat
+    {
+        public string Name { get; set; } //имя
+
+        public Cat(string name)
+        {
+            Name = name;
+        }
+       
+
+        public string GetInfo()
+        {
+            return String.Format("Кот {0}", Name);
+        }
+
+        public string GetInfo(bool includeIntProperty)
+        {
+            if (includeIntProperty)
+                return GetInfo();
+            else
+                return String.Format("Кот {0}", Name);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Вася");
+            Console.WriteLine(cat.GetInfo() + "\n");
+            string name;
+            Console.Write("Введите имя кота: ");
+            name = Console.ReadLine();
+            
+            cat.Name = name;
+            Console.WriteLine();
+            Console.WriteLine(cat.GetInfo(true)); //вывод полной инфы
+            Console.WriteLine(cat.GetInfo(false)); //вывод инфы без возраста
+            Console.ReadKey();
+        }
+    }
+}
